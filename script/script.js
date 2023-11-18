c7103fd51b29658313d092439efa46fbbe8fbf42
Hamza Najfi <141533696+asli-nobita@users.noreply.github.com>
2023-11-19 00:12:25 +0530

    Update script.js

--- script/script.js
+++ script/script.js
@@ -3,8 +3,13 @@ document.addEventListener('DOMContentLoaded', ()=>{
     const mobileMenuBtn = document.getElementById('mobile-menu-btn');
     const burgerMenu = document.getElementById('burger-menu');
 
-    const toggleDropdown = function() {
-        burgerMenu.classList.toggle('show');
+    const toggleDropdown = function(menu) {
+        menu.classList.toggle('show');
+        if (cart.style.visibility === 'hidden') {
+            cart.style.visibility = 'visible';
+        } else {
+            cart.style.visibility = 'hidden';
+        }
     };
     mobileMenuBtn.addEventListener('click', function(e) {
         e.stopPropagation();
